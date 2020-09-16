d729b332b5afd2a036e31827a763fc3fa2201c68
Vitalii Skubii <svorder@ukr.net>
2020-09-17 00:02:20 +0300

    cvcdf

---
+++ train.js
@@ -0,0 +1,9 @@
+let invoice = 150;
+const stock = 100;
+
+// Write code under this line
+let message =
+  invoice > stock
+    ? 'На складе недостаточно товаров!'
+    : 'Заказ оформлен, с вами свяжется менеджер';
+console.log(message);
